b0cd14ef61a62ecdb770b884d0a27f149c598791
Sanzhar Kiyassov <kiyassovs@gmail.com>
2023-10-24 11:23:00 +0600

    Initial Commit

---
+++ tab_bar_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tab_bar_app
+//
+//  Created by Sanzhar Kiyassov on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
